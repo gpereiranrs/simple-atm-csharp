c93776e35588bb0cdcc58aa679ab76186f5a3a41
Gabriel Pereira <gpereiranrs@outlook.com>
2023-09-04 22:02:49 -0300

    classe display a ser implementada

--- ATM_Card.cs
+++ ATM_Card.cs
@@ -6,19 +6,12 @@ using System.Threading.Tasks;
 
 namespace ATM_App
 {
-    public class ATMCard
+    public class ATMCardValidate
     {
         public string clientName {  get; set; }
         public string[] cardNumber { get; set; }
         public int validationCardYear { get; set; }
-        public static void DisplayLogin()
-        {
-            ATMCard client1 = new ATMCard();
-            client1.clientName = "Gabriel";
-            client1.cardNumber = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
-            client1.validationCardYear = 2016;
-            client1.CardValidation(client1.cardNumber, client1.validationCardYear);
-        }
+
         public void CardValidation(string[] cardNumber, int cardYear)
         {
             if (cardNumber.GetLength(0) == 10)
--- Program.cs
+++ Program.cs
@@ -9,8 +9,12 @@ namespace ATM_App
     class Program
     {
         static void Main(string[] args) 
-        { 
-            ATMCard.DisplayLogin();
+        {
+            ATMCardValidate client1 = new ATMCardValidate();
+            client1.clientName = "Gabriel";
+            client1.cardNumber = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+            client1.validationCardYear = 2016;
+            client1.CardValidation(client1.cardNumber, client1.validationCardYear);
         }
     }
 }
